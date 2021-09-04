c9d6318fd36095b9caf403721b3b4d4327275f87
pflugoli <pflugoli@b-tu.de>
2021-09-04 14:47:54 +0200

    Add root privilege check

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ $EUID -ne 0 ]]; then
+   echo "Please run this file with root privileges." 
+   exit 1
+fi
+
 sudo rm /bin/dmenuXgit
 rm -rf /home/"$USER"/dmenuXgit
 rm ./setup.sh ./dmenuXgit ./uninstall.sh ./script.sh ./README.md ./LICENSE
