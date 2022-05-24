01fd44633e538f42a04b30eb3d715ad871e7434f
Camonophy <pflug-oliver@gmx.de>
2022-05-24 22:17:24 +0200

    Add git functionalities

--- dmenuXgit.go
+++ dmenuXgit.go
@@ -8,7 +8,7 @@ import (
 )
 
 const DXG_SOURCE = "/usr/share/dmenuxgit/"
-const DXG_OPT = "add\nclone\ncommit\npull\npush\nrestore\nstatus\nexit"
+const DXG_OPT = "add\nclone\ncommit\ndiff\npull\npush\nstatus\nFREE\nexit"
 
 var PATH string
 
@@ -20,34 +20,38 @@ func main() {
 		switch string(out) {
 		case "add\n":
 			add_process()
-
+			
 		case "clone\n":
 			clone_process()
-
+			
 		case "commit\n":
 			commit_process()
-
+			
 		case "diff\n":
-			diff_process()
-
+			diff_process()		// TOFIX
+			
+		case "FREE\n":
+			free_process()
+			
 		case "merge\n":
-			merge_process()
-
+			merge_process()		// TODO
+			
 		case "pull\n":
 			pull_process()
-
+			
 		case "push\n":
-			push_process()
-
+			push_process()		
+			
 		case "restore\n":
-			restore_process()
-
+			restore_process()	// TODO
+			
 		case "status\n":
-			status_process()
-
+			status_process()	
+			
 		default:
 			loop = false
 		}
+		fmt.Println("-------------------------------------------------------------------------------")
 	}
 }
 
@@ -55,7 +59,7 @@ func add_process() {
 	grep := bash("git status | grep \"nothing to commit, working tree clean\"")
 
 	if grep != "" {
-		bash("echo \"" + grep + "\"")
+		fmt.Println(grep)
 	} else {
 		// Read all modified files from the [git status] output
 		modifiedFiles := bash("git status | sed -E \"s,\\t|\\r|\\n,,g\" | grep \"^modified:\" | cut -d \":\" -f 2 | tr -d ' '")
@@ -63,34 +67,43 @@ func add_process() {
 		// Read all untracked files from the [git status] output
 		untrackedFileEnd, _ := strconv.ParseUint(strings.Replace(bash("git status | wc -l "), "\n", "", -1), 10, 32)
 		untrackedFileEnd -= 2
-		untrackedFileBegin := bash("echo $(git status | grep -n 'Untracked files:' | cut -d ':' -f 1) + 2 | bc")
-		untrackedFiles := bash("git status | head -" + strconv.FormatUint(untrackedFileEnd, 10) + " | tail +" + untrackedFileBegin + " | tr -d '\011' | cut -d ':' -f 2")
-		untrackedFiles = strings.Replace(untrackedFiles, "\t", "", -1)
+		untrackedFileBegin := bash("echo $(git status | grep -n 'Untracked files:' | cut -d ':' -f 1) + 3 | bc")
+		var untrackedFiles string = ""
+
+		if untrackedFileBegin != "" {
+			untrackedFiles = bash("git status | head -" + strconv.FormatUint(untrackedFileEnd, 10) + " | tail +" + untrackedFileBegin + " | tr -d '\011' | cut -d ':' -f 2")
+			untrackedFiles = strings.Replace(untrackedFiles, "\t", "", -1)
+		} 
 
 		// Merge file list strings
 		files := modifiedFiles + untrackedFiles
 
-		if files == "" {
-			bash("echo -e \"\" | dmenu -i -p \"No file to add!\"")
+		// Chose one of the listed files and [git add] it
+		addFile := bash("echo -e \"" + files + "\nALL\" | dmenu -i -p \"git add \" ")
+
+		if addFile == "ALL" {
+			bash("git add -A")
 		} else {
-
-			// Chose one of the listed files and [git add] it
-			addFile := bash("echo -e \"" + files + "\\nALL\" | dmenu -i -p \"git add \" ")
-
-			if addFile == "ALL" {
-				bash("git add -A")
-			} else {
-				bash("git add " + addFile)
-			}
-			fmt.Println(addFile)
+			bash("git add " + addFile)
 		}
 	}
 }
 
 // SSH support only
 func clone_process() {
-	//clonePath := bash("echo \"\" | dmenu -i -p \"Destination path \"")
+	//DXG_KEY="HTTPS\nSSH"
+	//DXG_CH=$(echo -e $DXG_KEY | dmenu -i -p "Clone options ")
+
+	DXG_DIR := strings.Replace(bash("echo \"\" | dmenu -i -p \"Destination path \""), "\n", "", -1)
+	DXG_OWNER := strings.Replace(bash("echo \"\" | dmenu -i -p \"Name of the repository owner \""), "\n", "", -1)
+	DXG_REPO := strings.Replace(bash("echo \"\" | dmenu -i -p \"Name of the repository project \""), "\n", "", -1)
+	DXG_SSH := strings.Replace(bash("echo \"\" | dmenu -i -p \"Path to public SSH key \""), "\n", "", -1)
 	
+	// Clone
+	bash("git clone git@github.com:" + DXG_OWNER + "/" + DXG_REPO + ".git  --config core.sshCommand=\"ssh -i " + DXG_SSH + "\" " + DXG_DIR + "/" + DXG_REPO)
+	
+	// Add to config file
+	bash("echo \"" + DXG_DIR + "/" + DXG_REPO + "\" >> /usr/share/dmenuxgit/dmenuxgit.conf")
 }
 
 func commit_process() {
@@ -99,7 +112,34 @@ func commit_process() {
 }
 
 func diff_process() {
+	grep := bash("git status | grep \"nothing to commit, working tree clean\"")
 
+	if grep != "" {
+		fmt.Println("No changes took place.")
+	} else {
+		// Read all modified files from the [git status] output
+		modifiedFiles := bash("git status | sed -E \"s,\\t|\\r|\\n,,g\" | grep \"^modified:\" | cut -d \":\" -f 2 | tr -d ' '")
+
+		// Read all untracked files from the [git status] output
+		untrackedFileEnd, _ := strconv.ParseUint(strings.Replace(bash("git status | wc -l "), "\n", "", -1), 10, 32)
+		untrackedFileEnd -= 2
+		untrackedFileBegin := bash("echo $(git status | grep -n 'Untracked files:' | cut -d ':' -f 1) + 2 | bc")
+		var untrackedFiles string = ""
+		
+		if untrackedFileBegin != "" {
+			untrackedFiles := bash("git status | head -" + strconv.FormatUint(untrackedFileEnd, 10) + " | tail +" + untrackedFileBegin + " | tr -d '\011' | cut -d ':' -f 2")
+			untrackedFiles = strings.Replace(untrackedFiles, "\t", "", -1)
+		}
+
+		// Merge file list strings
+		files := modifiedFiles + untrackedFiles
+
+		// Chose one of the listed files and [git add] it
+		addFile := bash("echo -e \"" + files + "\" | dmenu -i -p \"git diff \" ")
+
+		out := bash("git diff " + addFile + " | less")
+		fmt.Println(out)
+	}	
 }
 
 func pull_process() {
@@ -120,6 +160,13 @@ func restore_process() {
 
 func merge_process() {
 
+}
+
+// Put in bash commands
+func free_process() {
+	input := bash("echo \"\" | dmenu -i -p \"Write your command \"")
+	out := bash(input)
+	fmt.Println(out)
 }
 
 func bash(cmd string) string {
